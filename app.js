335885643a540dec14af12a942822a8e02cd3c1c
Mohsin Ali <mohsinali421@gmail.com>
2024-09-22 20:30:19 +0400

    third version

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = require("express")()
 app.get("/", (req,res) => {
-    res.json("This is version 2")
+    res.json("This is third version")
 })
 app.listen(3000, () => {
     console.log(`Server started at 3000...`);
