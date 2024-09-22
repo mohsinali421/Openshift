81bebbb681797e286f93d4601b7d600fbb6a63ea
Mohsin Ali <mohsinali421@gmail.com>
2024-09-22 20:45:05 +0400

    V4

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = require("express")()
 app.get("/", (req,res) => {
-    res.json("This is third version")
+    res.json("This is webhook version 4")
 })
 app.listen(3000, () => {
     console.log(`Server started at 3000...`);
